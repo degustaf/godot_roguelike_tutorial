fd833ef836af51b8eb531eaeee107c85ed55201b
Derek Gustafson <degustaf@gmail.com>
2026-03-31 09:18:10 -0400

    Finish tutorial part 4

--- src/Game/game.gd
+++ src/Game/game.gd
@@ -8,14 +8,15 @@ var player_grid_pos:= Vector2i.ZERO
 @onready var event_handler: EventHandler = $EventHandler
 @onready var entities: Node2D = $Entities
 @onready var map: Map = $Map
+@onready var camera: Camera2D = $Camera2D
 
 func _ready() -> void:
 	player = Entity.new(Vector2i.ZERO, player_definition)
-	var camera: Camera2D = $Camera2D
 	remove_child(camera)
 	player.add_child(camera)
 	entities.add_child(player)
 	map.generate(player)
+	map.update_fov(player.grid_position)
 
 func get_map_data() -> MapData:
 	return map.map_data
@@ -23,4 +24,7 @@ func get_map_data() -> MapData:
 func _physics_process(_delta: float) -> void:
 	var action: Action = event_handler.get_action()
 	if action:
+		var previous_player_position: Vector2i = player.grid_position
 		action.perform(self, player)
+		if player.grid_position != previous_player_position:
+			map.update_fov(player.grid_position)
