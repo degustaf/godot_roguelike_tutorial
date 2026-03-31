c564cd4c5663e32d4a63b611475a984438e8decf
Derek Gustafson <degustaf@gmail.com>
2026-03-31 07:51:31 -0400

    Finish tutorial step 3

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Godot Roguelike Tutorial
 
-This is  my walkthrough of [Selina](https://github.com/SelinaDev)'s [Godot Roguelike tutoriall](https://github.com/SelinaDev/Godot-Roguelike-Tutorial)
+This is  my walkthrough of [Selina](https://github.com/SelinaDev)'s [Godot Roguelike tutorial](https://github.com/SelinaDev/Godot-Roguelike-Tutorial)
--- src/Game/game.gd
+++ src/Game/game.gd
@@ -10,12 +10,12 @@ var player_grid_pos:= Vector2i.ZERO
 @onready var map: Map = $Map
 
 func _ready() -> void:
-	var player_start_pos: Vector2i = Grid.world_to_grid(get_viewport_rect().size.floor() / 2)
-	player = Entity.new(player_start_pos, player_definition)
+	player = Entity.new(Vector2i.ZERO, player_definition)
+	var camera: Camera2D = $Camera2D
+	remove_child(camera)
+	player.add_child(camera)
 	entities.add_child(player)
-	var npc = Entity.new(player_start_pos + Vector2i.RIGHT, player_definition)
-	npc.modulate = Color.ORANGE_RED
-	entities.add_child(npc)
+	map.generate(player)
 
 func get_map_data() -> MapData:
 	return map.map_data
